ff6abed13edca035da0a9654bd6a28c8a180e53c
TallysHenrike <tallys.henrike@gmail.com>
2019-08-06 15:39:03 -0300

    Tratamento de erro

--- src/app/restaurants/restaurants.services.ts
+++ src/app/restaurants/restaurants.services.ts
@@ -2,11 +2,13 @@ import { Http } from '@angular/http';
 
 import { Observable } from 'rxjs/Observable';
 import 'rxjs/add/operator/map';
+import 'rxjs/add/operator/catch';
 
 import {Restaurant} from './restaurant/restaurant.model';
 
 import { MEAT_API } from '../app.api';
 import { Injectable } from '@angular/core';
+import { ErrorHandler } from '../app.error-handler';
 
 @Injectable()
 export class RestaurantsService {
@@ -15,6 +17,7 @@ export class RestaurantsService {
 
     restaurants(): Observable<Restaurant[]> {
         return this.http.get(`${MEAT_API}/restaurants`)
-            .map(response => response.json());
+            .map(response => response.json())
+            .catch(ErrorHandler.handleError);
     }
 }
